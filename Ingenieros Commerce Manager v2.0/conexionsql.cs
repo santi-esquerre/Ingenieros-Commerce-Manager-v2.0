77b4ebdfd1892339b57355d1d4cfc364d43bc490
BuildTools <unconfigured@null.spigotmc.org>
2022-08-25 16:53:59 -0300

    Add project files.

---
+++ Ingenieros Commerce Manager v2.0/conexionsql.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Ingenieros_Commerce_Manager_v2._0
+{
+    public class conexionsql
+    {
+        public MySqlConnection conexion = new MySqlConnection("Server=localhost; Database=regsitro; Uid=root; Pwd=;");
+        public MySqlCommand comandos = new MySqlCommand();
+        public MySqlDataReader datos;
+    }
+}
